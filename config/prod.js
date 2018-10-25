9a6a33e8e84d2fc2140e051baf82944c9c5b0cd1
vqhung <vqhung@gmail.com>
2018-10-25 11:34:38 +1100

    Adding authetication feature

---
+++ config/prod.js
@@ -0,0 +1,7 @@
+// dev.js
+module.exports = {
+    googleClientID: process.env.GOOGLE_CLIENT_ID,
+    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    mongodbURI: process.env.MONGODB_URI,
+    cookieKey: process.env.COOKIE_KEY
+}
--- index.js
+++ index.js
@@ -1,12 +1,74 @@
 const express = require('express')
+const passport = require('passport')
+const cookieSession = require('cookie-session')
+const GoogleStrategy = require('passport-google-oauth20').Strategy
+const keys = require('./config/keys')
+const mongoose = require('mongoose')
+require('./models/User')
+
+mongoose.connect(keys.mongodbURI, { useNewUrlParser: true })
+
+const User = mongoose.model('users')
 
 const app = express()
 
-app.get('/', (req, res)=>{
-    res.send({hi: 'there'})
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
+    })
+)
+
+app.use(passport.initialize())
+
+app.use(passport.session())
+
+passport.serializeUser((user, done) => {
+    done(null, user.id)
 })
 
-const port = process.env.PORT || 4000
+passport.deserializeUser((id, done) => {
+    console.log(id)
+    User.findById(id)
+    .then(user => {
+        console.log(user)
+        done(null, user)
+    })
+})
+
+passport.use(new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback'
+}, (accessToken, refreshToken, profile, done) => {
+   User.findOne({googleId: profile.id}).then(existingUser => {
+       if (existingUser) {
+           done(null, existingUser)
+       } else {
+           new User({googleId: profile.id}).save().then(user => done(null, user))
+       }
+   } )
+}
+))
+
+
+app.get('/google/auth', passport.authenticate('google', {
+    scope: ['profile', 'email']
+}))
+
+app.get('/auth/google/callback', passport.authenticate('google'))
+
+app.get('/api/getUser', (req, res) => {
+    console.log(req.user)
+    res.send(req.user)
+})
+
+app.get('/api/logout', (req, res) => {
+    req.logOut()
+    res.send(req.user)
+})
+
+const port = process.env.PORT || 5000
 
 app.listen(port, ()=>{
     console.log(`Server is up and running at port ${port}`)
